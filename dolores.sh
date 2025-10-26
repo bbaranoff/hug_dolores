578228556763cbbe8c023f173fdafc866d766afd
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2025-10-26 19:13:47 +0100

    Change docker run option from -it to -t

--- dolores.sh
+++ dolores.sh
@@ -32,7 +32,7 @@ fi
 
 # Serve silencieux + prompt
 echo "[+] Lancement $IMAGE (modèle=$MODEL, port=$PORT)…"
-exec docker run -it --rm \
+exec docker run -t --rm \
   "${GPU_FLAG[@]}" \
   -p "$PORT:$PORT" \
   -v "$VOL":/root/.ollama \
