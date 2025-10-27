a5787451c7fa3202fb14d90f20d67f7cd46817cb
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2025-10-27 17:17:59 +0100

    Add NVIDIA container toolkit installation steps

--- dolores.sh
+++ dolores.sh
@@ -48,6 +48,10 @@ else
 fi
 
 # === Étape 2 : Détection GPU NVIDIA (optionnelle) ===
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    $SUDO tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 $SUDO  apt-get install -y nvidia-container-toolkit
 $SUDO nvidia-ctk runtime configure --runtime=docker
 $SUDO systemctl restart docker
